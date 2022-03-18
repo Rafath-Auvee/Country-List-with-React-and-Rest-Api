f3a996c6e192524e6ef85635025ca4ba0ca0d9ad
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-03-18 20:39:08 +0600

    auto import

--- src/App.js
+++ src/App.js
@@ -1,48 +1,52 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import Country from "./components/Country/Country";
+
 function App() {
+  
   return (
     <div className="App">
-      <LoadContries></LoadContries>
+      {/* <LoadContries></LoadContries> */}
+      <Country></Country>
     </div>
   );
 }
 
-function LoadContries(){
-  const [countries, setcountries] = useState([])
+// function LoadContries(){
+//   const [countries, setcountries] = useState([])
 
-  useEffect(() => {
+//   useEffect(() => {
     
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => setcountries(data))
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res => res.json())
+//     .then(data => setcountries(data))
 
     
-  }, [])
+//   }, [])
   
   
   
   
-  return(
-    <div>
-      <h1>All Countries Chart List</h1>
-      <h3>Avaliable Countries: {countries.length} </h3>
+//   return(
+//     <div>
+//       <h1>All Countries Chart List</h1>
+//       <h3>Avaliable Countries: {countries.length} </h3>
       
-      {
-        countries.map(country => <p> <Country name={country.name.common} population={country.population}></Country> </p> )
-      }
-    </div>
-  )
-}
+//       {
+//         countries.map(country => <p> <Country name={country.name.common} population={country.population}></Country> </p> )
+//       }
+//     </div>
+//   )
+// }
 
-function Country(props) {
-  return(
-    <div>
-      <h2>Name: {props.name}</h2>
-      <h4>Population: {props.population}</h4>
-    </div>
-  )
-}
+// function Country(props) {
+//   return(
+//     <div>
+//       <h2>Name: {props.name}</h2>
+//       <h4>Population: {props.population}</h4>
+//     </div>
+//   )
+// }
 
-export default App;
+// export default App;
